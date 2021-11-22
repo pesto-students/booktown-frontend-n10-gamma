fea57722b2fc287ea418d45e458996bd44883202
tejas96 <tejas96patil@gmail.com>
2021-11-22 22:04:39 +0530

    PR changes & resolve bugs

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './config/appTheme';
+import './config/firebase/firebaseconfig';
+import Store from './config/redux/store';
+import ErrorContextProvider from './contextProviders/ErrorContextProvider';
+import './index.css';
+import TheBookTownApplication from './router';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+const App = () => {
+  return (
+    <Provider store={Store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <ApolloProvider client={client}>
+            <ErrorContextProvider>
+              <TheBookTownApplication />
+            </ErrorContextProvider>
+          </ApolloProvider>
+        </ThemeProvider>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import theme from './config/appTheme';
+import App from './App';
 import './config/firebase/firebaseconfig';
-import Store from './config/redux/store';
-import ErrorContextProvider from './contextProviders/ErrorContextProvider';
 import './index.css';
-import TheBookTownApplication from './router';
-
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache()
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={Store}>
-      <Router>
-        <ThemeProvider theme={theme}>
-          <ApolloProvider client={client}>
-            <ErrorContextProvider>
-              <TheBookTownApplication />
-            </ErrorContextProvider>
-          </ApolloProvider>
-        </ThemeProvider>
-      </Router>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
