76d29decc84826b525bd3f43104a0e514b3b9d15
tejas96 <tejas96patil@gmail.com>
2021-11-27 21:41:56 +0530

    add eslint new rule, update button theme, add common styled components file

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,5 +3,22 @@ import Header from './header';
 import SearchBar from './search';
 import Input from './input';
 import Button from './button';
-
-export { Footer, Header, SearchBar, Input, Button };
+import * as CommonStyled from './styledComponents';
+import FileUploader from './fileUploader';
+import Select from './select';
+import CheckBox from './checkbox';
+import Radio from './radio';
+import InputError from './inputError';
+export {
+  Select,
+  Footer,
+  Header,
+  SearchBar,
+  Input,
+  Button,
+  CommonStyled,
+  FileUploader,
+  CheckBox,
+  Radio,
+  InputError
+};
