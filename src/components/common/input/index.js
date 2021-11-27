377e835cf07ccc1a5e8844945a42cdc755ad3776
tejas96 <tejas96patil@gmail.com>
2021-11-27 21:32:56 +0530

    add new input reusable component

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -1,43 +1,101 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { InputContainer } from './styledComponents';
-
+import {} from 'react-bootstrap';
+import { InputError } from '..';
+/**
+ *
+ * @param {{
+ * type: ('number'|'password'|'text'|'email'),
+ * name: String,
+ * value: String,
+ * placeholder: String,
+ * onChange: Function,
+ * error: String,
+ * label: String,
+ * className: String,
+ * preFix : any,
+ * postFix : any,
+ * multiline : Boolean,
+ * rows : Number,
+ * cols : Number,
+ * disabled : Boolean
+ * width: any
+ * }} param0
+ * @returns
+ */
 function Input({
-  type,
+  type = 'text',
   name,
   value,
-  onChange,
+  onChange = () => {},
   placeholder,
   error,
   label,
   className,
   preFix,
   postFix,
+  multiline = false,
+  rows = 5,
+  cols = 50,
+  disabled = false,
+  width = 'auto',
   ...props
 }) {
   return (
-    <InputContainer>
+    <InputContainer width={width}>
       <label className={error ? 'input-label-error' : ''} htmlFor={name}>
         {label}
       </label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className={className}
-        {...props}
-      />
-      {error && (
-        <div>
-          <span>{error}</span>
-        </div>
+      {multiline ? (
+        <textarea
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={className}
+          rows={rows}
+          cols={cols}
+          {...props}
+        />
+      ) : (
+        <>
+          <div className="input-container">
+            {preFix && <span className="input-prefix">{preFix}</span>}
+            <input
+              type={type}
+              name={name}
+              value={value}
+              onChange={onChange}
+              placeholder={placeholder}
+              className={className}
+              {...props}
+            />
+            {postFix && <span className="input-postfix">{postFix}</span>}
+          </div>
+        </>
       )}
+      <InputError error={error} />
     </InputContainer>
   );
 }
 
-Input.propTypes = {};
+Input.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  error: PropTypes.string,
+  label: PropTypes.string,
+  className: PropTypes.string,
+  preFix: PropTypes.any,
+  postFix: PropTypes.any,
+  rows: PropTypes.number,
+  cols: PropTypes.number,
+  disabled: PropTypes.bool,
+  multiline: PropTypes.bool,
+  width: PropTypes.any
+};
 
-export default Input;
+export default React.memo(Input);
--- src/components/common/input/styledComponents.js
+++ src/components/common/input/styledComponents.js
@@ -5,13 +5,14 @@ export const InputContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
+  width: ${(props) => (props.width ? props.width : 'auto')};
   .input-label-error {
     color: red;
   }
   input {
     box-sizing: border-box;
     padding: ${(props) => props.theme.size.sm};
-    width: 100%;
+    width: auto;
     border: none;
     outline: none;
     color: ${(props) => props.theme.colors.primaryLight};
@@ -19,7 +20,34 @@ export const InputContainer = styled.div`
     margin: 5px 0;
     background-color: ${(props) => props.theme.colors.secondary};
   }
+  textarea {
+    box-sizing: border-box;
+    resize: none;
+    padding: ${(props) => props.theme.size.sm};
+    width: auto;
+    border: none;
+    outline: none;
+    margin: 5px 0;
+    color: ${(props) => props.theme.colors.primaryLight};
+    border-radius: ${(props) => props.theme.size.xsm};
+    background-color: ${(props) => props.theme.colors.secondary};
+  }
   .signup-input {
     margin: 0 2px;
+  }
+  .input-container {
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    .input-prefix,
+    .input-postfix {
+      padding: ${(props) => props.theme.size.sm};
+      height: 100%;
+      width: 10%;
+      text-align: center;
+      box-sizing: border-box;
+    }
   }
 `;
