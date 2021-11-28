b78c8be1b5f2570e7832578b24518220e31ea9d8
tejas96 <tejas96patil@gmail.com>
2021-11-28 22:13:40 +0530

    refactor product listing page

--- src/components/ProductListingPage/ProductListingPage.js
+++ src/components/ProductListingPage/ProductListingPage.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Footer } from '../common';
 import Header from '../common/header';
-import Body from './Body';
-import SideBar from './SideBar';
-import { Container, ProductListing } from './styledComponents';
-
+import Card from './Card';
 function ProductListingPage() {
   return (
     <>
-    <Header />
-      <ProductListing>
-        <SideBar />
-        <Container>
-          <Body />
-        </Container>
-      </ProductListing>
+      <Header />
+      <ProductListingPageContainer>
+        {new Array(10).fill(0).map((item) => (
+          <Card
+            title={''}
+            description={''}
+            productPics={''}
+            originalPrice={''}
+          />
+        ))}
+      </ProductListingPageContainer>
       <Footer />
     </>
   );
 }
 
 export default ProductListingPage;
+
+export const ProductListingPageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  background: ${(props) => props.theme.colors.secondary};
+`;
