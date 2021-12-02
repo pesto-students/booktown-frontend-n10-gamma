e39343ac46ca4b2fbe0996b07b77860b7315a5a1
tejas96 <tejas96patil@gmail.com>
2021-12-02 21:37:28 +0530

    added loader

--- src/router/ProtectiveRouter.js
+++ src/router/ProtectiveRouter.js
@@ -1,5 +1,6 @@
 import 'firebase/compat/auth';
 import React, { useEffect, useState } from 'react';
+import Loader from 'react-loader-spinner';
 import { Route, useHistory } from 'react-router-dom';
 import useSession from '../hooks/useSession';
 
@@ -30,7 +31,23 @@ const ProtectiveRouter = (props) => {
   return (
     <>
       {loading ? (
-        <div>Loading...</div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            width: '100vw',
+            backgroundColor: '#263238'
+          }}>
+          <Loader
+            type="Circles"
+            color="#F5F5F6"
+            height={100}
+            width={100}
+            timeout={100000}
+          />
+        </div>
       ) : (
         isUserLogin && (
           <Route path={props.path} component={props.component} {...props} />
