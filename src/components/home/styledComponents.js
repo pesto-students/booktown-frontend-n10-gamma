8fbe9ab542c71d5483a0d5667259d5fa5b61aa1b
tejas96 <tejas96patil@gmail.com>
2021-12-03 23:40:45 +0530

    signup flow completed, home page image width issue fix

--- src/components/home/styledComponents.js
+++ src/components/home/styledComponents.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 export const HomeContainer = styled.div`
-  width: 99vw;
+  width: 100vw;
   height: 100vh;
   display: flex;
   position: relative;
   img {
     object-fit: fill;
     height: 100%;
-    width: 99vw;
+    width: 100vw;
   }
 `;
 
