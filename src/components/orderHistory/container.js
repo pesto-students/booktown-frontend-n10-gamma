26ff25fa62d29215f82ad05b12da0715de34c624
tejas96 <tejas96patil@gmail.com>
2021-12-09 12:08:54 +0530

    impliment orderhistory container logic, update order history component

--- src/components/orderHistory/container.js
+++ src/components/orderHistory/container.js
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { getUserHistory } from '../../graphql/queries/order';
+import { useHistory } from 'react-router';
 /**
  *
- * @returns {object}
+ * @returns {
+ * userHistory:{
+ * loading: boolean,
+ * error: object,
+ * data: object,
+ * }
+ * }
  */
-const useOrderHistoryComponent = () => {
-  return {};
+const useOrderHistoryContainer = () => {
+  const history = useHistory();
+  const getUserHistoryQuery = useQuery(getUserHistory);
+  const [userHistory, setUserHistory] = useState({
+    data: [],
+    error: null,
+    loading: true
+  });
+
+  useEffect(() => {
+    if (getUserHistoryQuery.data && !getUserHistoryQuery.loading) {
+      setUserHistory(() => ({
+        data: getUserHistoryQuery.data?.getUserHistory?.data,
+        error: null,
+        loading: false
+      }));
+    }
+    if (getUserHistoryQuery.error && !getUserHistoryQuery.loading) {
+      setUserHistory(() => ({
+        data: [],
+        error: getUserHistoryQuery.error,
+        loading: false
+      }));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getUserHistoryQuery.loading]);
+
+  const handleRedirectToProductDetails = (productId) => {
+    history.push(`/product-details/${productId}`);
+  };
+  return {
+    userHistory,
+    handleRedirectToProductDetails
+  };
 };
 
-export default useOrderHistoryComponent;
+export default useOrderHistoryContainer;
