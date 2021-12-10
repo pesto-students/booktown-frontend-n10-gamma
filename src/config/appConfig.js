b086b5e013e119a81d4dae2c820242e73789d499
tejas96 <tejas96patil@gmail.com>
2021-12-10 22:58:04 +0530

    delete filter option and added in appConfig file

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -146,3 +146,30 @@ export const language = [
   'Maithili',
   'Marwari'
 ];
+
+export const filterOptions1 = [
+  {
+    header: 'Language',
+    options: [
+      { text: 'English', id: 1 },
+      { text: 'Hindi', id: 2 },
+      { text: 'Marathi', id: 3 },
+      { text: 'Tamil', id: 4 }
+    ]
+  },
+  {
+    header: 'Condition',
+    options: [
+      { text: 'New', id: 5 },
+      { text: 'Old', id: 6 }
+    ]
+  },
+  {
+    header: 'Format',
+    options: [
+      { text: 'Audio', id: 7 },
+      { text: 'Hard Copy', id: 8 },
+      { text: 'E-Book', id: 9 }
+    ]
+  }
+];
